0d51ae065c39f90c4308189b6652973019fb9744
catiulll <satriafzal@gmail.com>
2026-05-08 22:23:34 +0700

    update altaf

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
+            $table->string('username')->unique();
+            $table->string('no_telp')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('foto_profil')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
