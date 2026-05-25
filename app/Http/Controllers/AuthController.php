521403e98bef60fa316f7cd854096ebdb04617b0
catiulll <satriafzal@gmail.com>
2026-05-25 22:49:09 +0700

    bikin auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // LOGIC REGISTER
+    public function registerPost(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'nama' => $request->username,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'no_telp' => '-'.time(), 
+            'alamat' => '-',
+            'foto_profil' => 'default.png',
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Berhasil mendaftar!');
+    }
+
+    // LOGIC LOGIN
+    public function loginPost(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'Login berhasil!');
+        }
+
+        return back()->with('error', 'Email atau Password salah!');
+    }
+
+    // LOGIC LOGOUT
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('success', 'Berhasil logout!');
+    }
+}
