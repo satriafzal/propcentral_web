4bf5e24df0cf68d303d4a64709342e9e30272414
catiulll <satriafzal@gmail.com>
2026-05-31 21:53:29 +0700

    update routes for security acces permisons

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/contact', function (){
     return view('contact');
-});
-
-Route::get('/profile', function () {
-    return view('profile');
 });
 
 Route::get('/register', function () {
@@ -21,14 +18,6 @@ Route::get('/panduan', function () {
     return view('panduan');
 });
 
-Route::get('/jual', function () {
-    return view('jual');
-});
-
-Route::get('/property', function () {
-    return view('property');
-});
-
 Route::get('/saved', function () {
     return view('saved');
 });
@@ -36,4 +25,24 @@ Route::get('/saved', function () {
 // routes for authentication
 Route::post('/register', [AuthController::class, 'registerPost'])->name('register.post');
 Route::post('/login', [AuthController::class, 'loginPost'])->name('login.post');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// acces role permissions
+Route::middleware('auth')->group(function () {
+
+    Route::get('/profile', function () {
+        return view('profile');
+    });
+
+    Route::get('/property', function () {
+        return view('property');
+    });
+
+    Route::get('/jual', function () {
+        return view('jual');
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    // for update profile user
+    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+});
